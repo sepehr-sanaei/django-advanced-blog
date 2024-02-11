df6d392022fd0e1bb8fc7cbfcbfd65bb903ac32d
sepehr-sanaei <sepehrsanaeiazad@gmail.com>
2024-02-11 12:58:10 +0330

    Delete tempCodeRunnerFile.py

--- core/blog/tests/tempCodeRunnerFile.py
+++
@@ -1,6 +0,0 @@
-
-        api_client.force_authenticate(user=user)
-        response = api_client.post(url, data)
-        print(response.content)
-        assert response.status_code == 201
-        
