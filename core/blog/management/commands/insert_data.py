5f8016d6728dc4141ff0c392fc5eceaea499932a
sepehr-sanaei <sepehrsanaeiazad@gmail.com>
2024-02-12 17:56:23 +0330

    added dummy data to the project

---
+++ core/blog/management/commands/insert_data.py
@@ -0,0 +1,45 @@
+from io import StringIO
+from django.core.management.base import BaseCommand
+from faker import Faker
+from accounts.models import User, Profiles
+from blog.models import Post, Category
+import random
+from datetime import datetime
+
+
+
+category_list = [
+    'IT',
+    'PC',
+    'Fun'
+]
+
+class Command(BaseCommand):
+    help = "inserting dummy data"
+    
+    def __init__(self, *args, **kwargs):
+        super(Command, self).__init__(*args, **kwargs)
+        self.fake = Faker()
+
+    def handle(self, *args, **options):
+        user = User.objects.create_user(email=self.fake.email(), password='Test@12345')
+        profile = Profiles.objects.get(user=user)
+        profile.first_name = self.fake.first_name()
+        profile.last_name = self.fake.last_name()
+        profile.discription = self.fake.paragraph(nb_sentences=5)
+        profile.save()
+        
+        for name in category_list:
+            Category.objects.get_or_create(name=name)
+        
+        for _ in range(10):
+            Post.objects.create(
+                author = profile,
+                title = self.fake.paragraph(nb_sentences=1),
+                content = self.fake.paragraph(nb_sentences=10),
+                status = random.choice([True, False]),
+                category = Category.objects.get(name=random.choice(category_list)),
+                published_date = datetime.now()
+            )
+            
+        
